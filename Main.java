4cfd40b468e1026d72af9557e06d1655181c8b55
Ajay <ajaythakare500@gmail.com>
2026-03-18 17:01:07 +0530

    main file for first code
    i learned about the basic syntax and comments

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        // This is the first java program
+
+        /*
+        This
+        is a multiline
+        comment
+         */
+        System.out.println("I like Pizza!");
+        System.out.println("It's really good!");
+        System.out.print("Buy me Pizza!\n");
+    }
+}
