ac1beb355ffd0f0e1b55b3a598c5fa674a44d49b
Ajay <ajaythakare500@gmail.com>
2026-04-04 19:15:31 +0530

    Add files via upload

---
+++ arithmatico.java
@@ -0,0 +1,28 @@
+public class arithmatico {
+    public static void main (String[] args){
+        int x = 10;
+        int y = 2;
+        int z ;
+
+        /* {z = x + y;
+        z = x - y;
+       z = x * y;
+        z = x / y;
+        z = x % y;} /*
+
+        /* Augmented Assignment Operators
+
+        x += y;x=12
+        x -= y;x=8
+        x *= y;
+        x /= y;
+         */
+        /* Increment and Decrement operators
+           int x = 1;
+           x++; (x= x+ 1)
+           x--; x= x-1
+         */
+
+        // ORDER OF OPERATIONS [P-E-N-D-A-S]
+    }
+}
---
+++ shoppingcart.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class shoppingcart {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("What item would you like to buy?: ");
+        String item = scanner.nextLine();
+        System.out.println("What is the price for each?: ");
+        double cost = scanner.nextDouble();
+        System.out.println("How many would you like?: ");
+        int quantity = scanner.nextInt();
+        double total = cost * quantity;
+        System.out.println("You have bought "+quantity+" " +item +"/s");
+        System.out.println("Your total is $"+total);
+
+    }
+}
